bd1c3d72629c7b1306a5c478d2730c409eb55e05
MJ <mereana.johnston99@gmail.com>
2020-05-26 09:08:58 +1200

    terrain asset and basic movement code
    sourced and imported an artic tundra ice terrain assets, set up player and camera on new terrain and fixed movememnt code architecture.

---
+++ Assets/Scripts/SC_camera_follow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_camera_follow : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+    //access player's position
+
+    private Vector3 _cameraOffset;
+    //access offset of camera compared to player
+
+    [Range(0.01f, 1.0f)]
+    public float Smoothing = 0.5f;
+    //smooths camera following movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+        //assigns fixed offset of camera based on awake position
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    // instructs camera to follow with offset and lerp between current and new position for smooth movement
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, Smoothing);
+    }
+}
---
+++ Assets/Scripts/SC_phone_input_player_controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_phone_input_player_controller : MonoBehaviour
+{
+    public float speed;
+    private new Rigidbody rigidbody;
+
+    float xPos = Input.acceleration.y;
+    float yPos = Input.acceleration.x;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        Debug.Log("RB accessed");
+        
+
+    }
+    private void Update()
+    {
+
+        //creates a magnitude based speed variable so that the character moves faster at higher phone angles
+        //creates a vector between the xpos input and ypos input
+        Vector2 Magnitude = new Vector2(xPos, yPos);
+        //set magnitude vector to an absolute float value
+        float speedMultiplier = Mathf.Abs(Magnitude.magnitude);
+
+
+        speed = 15f * speedMultiplier;
+    }
+
+    void FixedUpdate()
+    {
+        //rotates the character based on the direction the phone is tilted towards
+        // creates the target rotation based on the phone's x and y position
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(xPos, 0, yPos));
+        // creates a slerp between the target rotation and current rotation. Might speed it up later, have a variable ready to do so.
+        Quaternion moveRotation = Quaternion.Slerp(rigidbody.rotation, targetRotation, Time.fixedDeltaTime);
+
+        rigidbody.MoveRotation(moveRotation);
+
+        rigidbody.AddForce(transform.forward * speed);
+
+    }
+}
