039e03c8aa483a1dc84f175ff2d8f5392f3380b2
MJ <mereana.johnston99@gmail.com>
2020-06-09 15:11:33 +1200

    Respawn
    started making a respawn function, fixed a few issues with the terrain collider

--- Assets/Scripts/SC_camera_follow.cs
+++ Assets/Scripts/SC_camera_follow.cs
@@ -6,19 +6,22 @@ public class SC_camera_follow : MonoBehaviour
 {
 
     public Transform PlayerTransform;
-    //access player's position
 
-    private Vector3 _cameraOffset;
+    private Vector3 cameraOffset;
     //access offset of camera compared to player
 
     [Range(0.01f, 1.0f)]
     public float Smoothing = 0.5f;
     //smooths camera following movement
+   
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        _cameraOffset = transform.position - PlayerTransform.position;
+        cameraOffset = transform.position - PlayerTransform.position;
+       
+       
         //assigns fixed offset of camera based on awake position
     }
 
@@ -26,8 +29,14 @@ public class SC_camera_follow : MonoBehaviour
     void LateUpdate()
     // instructs camera to follow with offset and lerp between current and new position for smooth movement
     {
-        Vector3 newPos = PlayerTransform.position + _cameraOffset;
-
+        Vector3 newPos = PlayerTransform.position + cameraOffset;
         transform.position = Vector3.Slerp(transform.position, newPos, Smoothing);
+       
+       
+        // instructs camera to follow with offset and lerp between current and new position for smooth movement
+
+
+
+
     }
 }
--- Assets/Scripts/SC_phone_input_player_controller.cs
+++ Assets/Scripts/SC_phone_input_player_controller.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class SC_phone_input_player_controller : MonoBehaviour
 {
-    public float speed = 15f;
+    public float speed = 20f;
     private new Rigidbody rigidbody;
 
-    float xPos = Input.acceleration.y;
-    float yPos = Input.acceleration.x;
-
+    float xPos;
+    float yPos;
+    private Vector3 lastPos;
+    bool isMoving;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
 
         Debug.Log("RB accessed");
-        
+
+         xPos = Input.acceleration.y;
+         yPos = Input.acceleration.x;
 
     }
-  
+
 
     void FixedUpdate()
     {
 
+        lastPos = transform.position;
+
         //rotates the character based on the direction the phone is tilted towards
         // creates the target rotation based on the phone's x and y position
         Quaternion targetRotation = Quaternion.LookRotation(new Vector3(xPos, 0, yPos));
-        // creates a slerp between the target rotation and current rotation. Might speed it up later, have a variable ready to do so.
+        // creates a slerp between the target rotation and current rotation.
         Quaternion moveRotation = Quaternion.Slerp(rigidbody.rotation, targetRotation, Time.fixedDeltaTime);
 
         //creates a magnitude based speed variable so that the character moves faster at higher phone angles
@@ -40,8 +45,25 @@ public class SC_phone_input_player_controller : MonoBehaviour
         float movementSpeed = speed * speedMultiplier;
 
         rigidbody.MoveRotation(moveRotation);
+        Debug.Log("I am spinning");
 
-        rigidbody.AddForce(transform.forward * movementSpeed);
+        rigidbody.AddForce(transform.position + transform.forward * movementSpeed);
+        Debug.Log("Ice is slippery!");
 
+        if(transform.position != lastPos)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+            Invoke("Respawn", 5f);
+        }
+
+    }
+
+    void Respawn (Vector3 checkpoint)
+    {
+        rigidbody.position = checkpoint;
     }
 }
