3a216145087a64ec5f9852f792051c9164f7720e
MJ <mereana.johnston99@gmail.com>
2020-06-09 16:12:12 +1200

    Edited Movement
    just fixed movement and rotation, both work much better now

--- Assets/Scripts/SC_phone_input_player_controller.cs
+++ Assets/Scripts/SC_phone_input_player_controller.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class SC_phone_input_player_controller : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 30f;
     private new Rigidbody rigidbody;
 
-    float xPos;
-    float yPos;
+    
     private Vector3 lastPos;
+    private Vector3 currentPos;
     bool isMoving;
 
     void Start()
@@ -18,47 +18,52 @@ public class SC_phone_input_player_controller : MonoBehaviour
 
         Debug.Log("RB accessed");
 
-         xPos = Input.acceleration.y;
-         yPos = Input.acceleration.x;
-
+       
     }
 
 
     void FixedUpdate()
     {
 
-        lastPos = transform.position;
+        currentPos = transform.position;
 
         //rotates the character based on the direction the phone is tilted towards
         // creates the target rotation based on the phone's x and y position
-        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(xPos, 0, yPos));
-        // creates a slerp between the target rotation and current rotation.
+     
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(Input.acceleration.x, 0, Input.acceleration.y));
+        // creates a slerp between the target rotation and current rotation. Might speed it up later, have a variable ready to do so.
         Quaternion moveRotation = Quaternion.Slerp(rigidbody.rotation, targetRotation, Time.fixedDeltaTime);
-
-        //creates a magnitude based speed variable so that the character moves faster at higher phone angles
-        //creates a vector between the xpos input and ypos input
-        Vector2 Magnitude = new Vector2(xPos, yPos);
-        //set magnitude vector to an absolute float value
-        float speedMultiplier = Mathf.Abs(Magnitude.magnitude);
-
-
-        float movementSpeed = speed * speedMultiplier;
 
         rigidbody.MoveRotation(moveRotation);
         Debug.Log("I am spinning");
 
-        rigidbody.AddForce(transform.position + transform.forward * movementSpeed);
+        //creates a magnitude based speed variable so that the character moves faster at higher phone angles
+        //creates a vector between the xpos input and ypos input
+        Vector2 Magnitude = new Vector2(Input.acceleration.x, Input.acceleration.y);
+        //set magnitude vector to an absolute float value
+        float speedMultiplier = Mathf.Abs(Magnitude.magnitude);
+
+
+       
+
+       
+
+        rigidbody.AddForce( transform.forward * speed *speedMultiplier);
         Debug.Log("Ice is slippery!");
 
-        if(transform.position != lastPos)
+        if(currentPos != lastPos)
         {
             isMoving = true;
+            Debug.Log("real slippery!");
         }
         else
         {
             isMoving = false;
-            Invoke("Respawn", 5f);
+            
+            //Invoke("Respawn", 5f);
         }
+
+        lastPos = currentPos;
 
     }
 
